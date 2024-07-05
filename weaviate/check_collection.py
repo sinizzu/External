10828b54fc38b6a6fb5949776357f7e683c66fb7
raminicano <raminicayes@gmail.com>
2024-07-05 07:50:39 +0000

    Chore: weaviate collection create

---
+++ weaviate/check_collection.py
@@ -0,0 +1,27 @@
+import weaviate
+import os
+from weaviate.classes.config import Property, DataType
+
+# 환경 변수 가져오기
+URL = os.getenv("WCS_URL")
+APIKEY = os.getenv("WCS_API_KEY")
+HUGGING = os.getenv("HUGGINGFACE_API_KEY")
+
+
+client = weaviate.connect_to_wcs(
+    cluster_url=URL,
+    auth_credentials=weaviate.auth.AuthApiKey(APIKEY),
+    headers={'X-HuggingFace-Api-Key': HUGGING}
+    )
+
+
+# 연결 확인
+if client.is_ready():
+    print("Weaviate Cloud에 성공적으로 연결되었습니다.")
+else:
+    print("Weaviate Cloud에 연결할 수 없습니다.")
+
+
+client.collections.create_from_dict()
+
+client.close()
