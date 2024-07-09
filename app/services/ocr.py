cd9286d4f5abaf76fcfe3db3004680da50723f8c
wjdguswn1203 <wjdguswn0954@gmail.com>
2024-07-09 08:50:25 +0000

    Feat: Organize full text by extracting abstract, introduction, and conclusion

--- app/services/ocr.py
+++ app/services/ocr.py
@@ -6,6 +6,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
 from app.db.connect_db import get_weaviate_client
 from weaviate.classes.query import Filter
+import re
 
 # Google Cloud Vision API 인증을 위한 환경 변수 설정
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./ocr_key.json"
@@ -144,9 +145,41 @@ async def searchFulltext(title: str):
         if response.objects:
             for object in response.objects:
                 res.append(object.properties) # 반환 데이터에 추가
+            fullTexts = res[0].get("texts")
+            res = process_text(fullTexts)
+            # res = fullTexts
             return {"resultCode" : 200, "data" : res}
         else:
             return {"resultCode" : 400, "data" : response}
     except Exception as e:
         return {"resultCode": 500, "data": str(e)}
+
+ # 전체 텍스트를 초록, 서론, 결론 추출
+def process_text(text):
+    try:
+        # Define regex patterns for abstract, introduction, and conclusion
+        abstract_pattern = re.compile(r'(?i)abstract\s*(.*?)(?=(introduction|1\sIntroduction))', re.DOTALL)
+        introduction_pattern = re.compile(r'(?i)(introduction|1\sIntroduction)\s*(.*?)(?=\\n\\n2\s)', re.DOTALL)
+        conclusion_pattern = re.compile(r'(?i)(conclusion|6\sConclusion)\s*(.*)', re.DOTALL)
+        
+        # Find matches
+        abstract_match = abstract_pattern.search(text)
+        introduction_match = introduction_pattern.search(text)
+        conclusion_match = conclusion_pattern.search(text)
+        
+        # Extract matched sections
+        abstract = abstract_match.group(1).strip() if abstract_match else "Abstract not found."
+        introduction = introduction_match.group(2).strip() if introduction_match else "Introduction not found."
+        conclusion = conclusion_match.group(2).strip() if conclusion_match else "Conclusion not found."
+        
+        response = {
+            "abstract": abstract,
+            "introduction": introduction,
+            "conclusion": conclusion
+        }
+
+        return response
+    except Exception as e:
+        print(f"Error processing text: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
 
