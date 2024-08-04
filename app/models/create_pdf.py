b1d31b778c8f6781e85549bf29c2d99caa46a3c4
raminicano <raminicayes@gmail.com>
2024-08-04 05:02:24 +0000

    Chore: 컬렉션 속성 수정

--- app/models/create_pdf.py
+++ app/models/create_pdf.py
@@ -38,7 +38,7 @@ client.collections.create(
     properties=[
         wc.Property(name="pdf_id", data_type=wc.DataType.TEXT),  # S3 ID or Paper collection object ID
         wc.Property(name="pdf_link", data_type=wc.DataType.TEXT),  # Direct link to the PDF
-        wc.Property(name="pre_image", data_type=wc.DataType.BLOB),  # Thumbnail image as blob
+        # wc.Property(name="pre_image", data_type=wc.DataType.BLOB),  # Thumbnail image as blob
         wc.Property(name="keywords", data_type=wc.DataType.TEXT_ARRAY),  # Extracted keywords
         wc.Property(name="full_text", data_type=wc.DataType.TEXT),  # Full text from OCR
         wc.Property(name="trans_summary", data_type=wc.DataType.TEXT),
